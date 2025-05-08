da61fb01b2338a9e19a84265ad19f9cf6ddb0672
xjapn03 <juanesmako3707@gmail.com>
2025-05-08 12:43:55 -0500

    first commit

---
+++ DML.sql
@@ -0,0 +1,17 @@
+/*Tablas Fuertes*/
+INSERT INTO Tip_Doc  (ID_Tip_Doc,Tipo) values (1,"Cedula,de,ciudadania"),(2,"Tarjeta,de,Identidad"),(3, "Tarjeta,de,Extranjeria"),(4,"Pasaporte"),(5,"Carne,Diplomatico");
+INSERT INTO Rol (ID_Rol,Nom) values (1,"Selador"),(2,"Cliente");
+INSERT INTO Tip_Pago  (ID_Tip_Pago,Tipo) values (1,"Pse"),(2,"Tarjeta,Credito"),(3, "Tarjeta,Debito"),(4,"Nequi"),(5,"Efectivo");
+INSERT INTO Barrio  (ID_Barrio,Nom) values (1,"Madelena"),(2,"Quirigua"),(3,"Patio,Bonito"),(4,"Candelaria"),(5,"Villa,Mayor");
+INSERT INTO Vias  (ID_Vias,Vias) values (1,"carrera"),(2,"calle"),(3,"Autopista"),(4,"Transversal");
+INSERT INTO Tip_Vehi (ID_Tip_Vehi,Tipo,Val_Hora,Val_Dia) values (1,"Automoviles","3.000","20.000"),(2,"Motos","2.300","11.200"),(3,"Autobuses-Camiones","4.500","30.000"),(4,"Bicicleta","700","50.000");
+INSERT INTO Sector (ID_Sector,Color) values (1,"Blanco"), (2,"Rojo"),(3,"Verde"),(4,"Azul");
+INSERT INTO Marca_Vehi (ID_Marca_Vehi,Marca) Values (1,"Renault"),(2,"Chevrolet"),(3,"Mazda"),(4,"Toyota"),(5,"Nissan"),(6,"Suzuki"),(7,"Honda"),(8,"Yamaha"),(9,"Suzuki"),(10,"Volvo");
+
+/*Tablas Debiles*/
+INSERT INTO Usuarios (ID_Usuarios,Nombre,Apellidos,Telefono,Correo,Num_Doc,ID_Tip_Doc,ID_Rol) values (1,"Lucia","Castañeda Lozano","3202189375","lcicastañeda@gmail.com","1234567892",1,2);
+INSERT INTO Vehiculo (ID_Vehiculo,Placa,Color,ID_Usuarios,ID_Tip_Vehi,ID_Marca_Vehi) values(1,"VKO34D","Rojo",1,2,6);
+INSERT INTO Ingresos (ID_Ingresos,Fech_Hor_Ing,Fech_Hor_Sal,Val_Pagado,ID_Usuarios,ID_Vehiculo) values (1,"2025-05-06 12:05:00","2025-05-06 15:20:04","2.800",1,1);
+INSERT INTO Direccion (ID_Direccion,ID_Vias,Numero,Complemento,ID_Barrio) values (1,2,"45","28 D-08",3);
+INSERT INTO Parqueadero (ID_Parqueadero,Nombre,Cap_Disp,Cap_Total,ID_Direccion) values (1,"ParkZone 24","20","25",1);
+INSERT INTO espacio (ID_espacio,Numero,Estado,ID_Tip_Vehi,ID_Sector,ID_Parqueadero) Values (1,"250",1,3,2,1);
---
+++ Parqueadero.sql
@@ -0,0 +1,150 @@
+DROP DATABASE IF EXISTS parqueadero;
+CREATE DATABASE parqueadero;
+USE parqueadero;
+
+CREATE TABLE Usuarios(
+ID_Usuarios INT PRIMARY KEY auto_increment,
+Nombre VARCHAR(40) NOT NULL,
+Apellidos VARCHAR(40) NOT NULL,
+Telefono VARCHAR(30) NOT NULL,
+Correo VARCHAR(50) NOT NULL,
+Num_Doc INT NOT NULL,
+ID_Tip_Doc INT,
+ID_Rol INT
+);
+
+CREATE TABLE Tip_Doc(
+ID_Tip_Doc INT PRIMARY KEY auto_increment,
+Tipo VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Rol(
+ID_Rol INT PRIMARY KEY auto_increment,
+Nom VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Marca_Vehi(
+ID_Marca_Vehi INT PRIMARY KEY auto_increment,
+Marca VARCHAR(50)
+);
+
+CREATE TABLE Tip_Vehi(
+ID_Tip_Vehi INT PRIMARY KEY auto_increment,
+Tipo VARCHAR(30) NOT NULL,
+Val_Hora DECIMAL (10,2) NOT NULL,
+Val_Dia DECIMAL (10,2) NOT NULL
+);
+
+CREATE TABLE Vehiculo(
+ID_Vehiculo INT PRIMARY KEY auto_increment,
+Placa VARCHAR(20) NOT NULL UNIQUE,
+Color VARCHAR(20) NOT NULL,
+ID_Marca_Vehi INT,
+ID_Usuarios INT,
+ID_Tip_Vehi INT
+);
+
+CREATE TABLE Tip_Pago(
+ID_Tip_Pago INT PRIMARY KEY auto_increment,
+Tipo VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Ingresos(
+ID_Ingresos INT PRIMARY KEY auto_increment,
+Fech_Hor_Ing timestamp NOT NULL DEFAULT current_timestamp,
+Fech_Hor_Sal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+Val_Pagado DECIMAL (10,2) NOT NULL,
+ID_Usuarios INT,
+ID_Vehiculo INT,
+ID_Tip_Pago INT
+);
+
+CREATE TABLE Barrio(
+ID_Barrio INT PRIMARY KEY auto_increment,
+Nom VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Vias(
+ID_Vias INT PRIMARY KEY auto_increment,
+Vias VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Direccion(
+ID_Direccion INT PRIMARY KEY auto_increment,
+Numero INT NOT NULL,
+Complemento VARCHAR(50) NOT NULL,
+ID_Barrio INT,
+ID_Vias INT
+);
+
+CREATE TABLE Parqueadero(
+ID_Parqueadero INT PRIMARY KEY auto_increment,
+Nombre VARCHAR(40) NOT NULL,
+Cap_Disp INT NOT NULL,
+Cap_Total INT NOT NULL,
+ID_Direccion INT
+);
+
+CREATE TABLE Sector(
+ID_Sector INT PRIMARY KEY auto_increment,
+Color VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE espacio(
+ID_espacio INT PRIMARY KEY auto_increment,
+Numero INT NOT NULL,
+Estado BINARY(1) NOT NULL,
+ID_Tip_Vehi INT,
+ID_Sector INT,
+ID_Parqueadero INT
+);
+
+/*Foreign Key*/
+ALTER TABLE Usuarios
+ADD CONSTRAINT FK_Tip_Doc
+FOREIGN KEY (ID_Tip_Doc) REFERENCES Tip_Doc(ID_Tip_Doc);
+ALTER TABLE Usuarios
+ADD CONSTRAINT FK_Rol
+FOREIGN KEY (ID_Rol) REFERENCES Rol(ID_Rol);
+
+ALTER TABLE Vehiculo 
+ADD CONSTRAINT FK_Marca_Vehi
+FOREIGN KEY (ID_Marca_Vehi) REFERENCES Marca_Vehi(ID_Marca_Vehi);
+ALTER TABLE Vehiculo
+ADD CONSTRAINT FK_Usuarios
+FOREIGN KEY (ID_Usuarios) REFERENCES Usuarios(ID_Usuarios);
+ALTER TABLE Vehiculo
+ADD CONSTRAINT FK_Tip_Vehi
+FOREIGN KEY (ID_Tip_Vehi) REFERENCES Tip_Vehi(ID_Tip_Vehi);
+
+ALTER TABLE Ingresos
+ADD CONSTRAINT Usuarios
+FOREIGN KEY (ID_Usuarios) REFERENCES Usuarios(ID_Usuarios);
+ALTER TABLE Ingresos
+ADD CONSTRAINT ID_Vehiculo
+FOREIGN KEY (ID_Vehiculo) REFERENCES Vehiculo(ID_Vehiculo);
+ALTER TABLE Ingresos
+ADD CONSTRAINT FK_Tip_Pago
+FOREIGN KEY (ID_Tip_Pago) REFERENCES Tip_Pago(ID_Tip_Pago);
+
+ALTER TABLE Direccion
+ADD CONSTRAINT FK_Barrio
+FOREIGN KEY (ID_Barrio) REFERENCES Barrio(ID_Barrio);
+ALTER TABLE Direccion
+ADD CONSTRAINT FK_Vias
+FOREIGN KEY (ID_Vias) REFERENCES Vias(ID_Vias);
+
+ALTER TABLE Parqueadero
+ADD CONSTRAINT FK_Direccion
+FOREIGN KEY (ID_Direccion) REFERENCES Direccion(ID_Direccion);
+
+ALTER TABLE espacio
+ADD CONSTRAINT FK_Tip_Vehic
+FOREIGN KEY (ID_Tip_Vehi) REFERENCES Tip_Vehi(ID_Tip_Vehi);
+
+ALTER TABLE espacio
+ADD CONSTRAINT FK_Sector
+FOREIGN KEY (ID_Sector) REFERENCES Sector(ID_Sector);
+ALTER TABLE espacio
+ADD CONSTRAINT FK_Parqueadero
+FOREIGN KEY (ID_Parqueadero) REFERENCES Parqueadero(ID_Parqueadero);
